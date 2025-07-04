c579eee21608d5a7eac16fd9b5a3a9c73707d91e
Nhan Lam <nhanltdf8@fullstack.edu.vn>
2025-07-04 15:03:08 +0700

    initial commit

---
+++ player.js
@@ -0,0 +1,47 @@
+const player = {
+  songs: [
+    {
+      id: 1,
+      name: "Bang Bang Bang",
+      singer: "Bigbang",
+      path: "./songs/Bang Bang Bang.mp3",
+    },
+
+    {
+      id: 2,
+      name: "Blue",
+      singer: "Bigbang",
+      path: "./songs/Blue.mp3",
+    },
+    {
+      id: 3,
+      name: "Fantastic Baby",
+      singer: "Bigbang",
+      path: "./songs/Fantastic Baby.mp3",
+    },
+    {
+      id: 4,
+      name: "Haru Haru",
+      singer: "Bigbang",
+      path: "./songs/Haru Haru.mp3",
+    },
+    {
+      id: 5,
+      name: "Still Life",
+      singer: "Bigbang",
+      path: "./songs/Still Life.mp3",
+    },
+    {
+      id: 6,
+      name: "Tell Me Goodbye",
+      singer: "Bigbang",
+      path: "./songs/Tell Me Goodbye.mp3",
+    },
+    {
+      id: 7,
+      name: "Tonight",
+      singer: "Bigbang",
+      path: "./songs/Tonight.mp3",
+    },
+  ],
+};
